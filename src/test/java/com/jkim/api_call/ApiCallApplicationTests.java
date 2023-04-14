bbefce82fe86b7c05e739c886cac62dde428463a
HelloJon29 <jonkim1996@gmail.com>
2023-04-13 21:43:00 -0500

    Generated Spring Boot project, added gitignore

---
+++ src/test/java/com/jkim/api_call/ApiCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkim.api_call;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
