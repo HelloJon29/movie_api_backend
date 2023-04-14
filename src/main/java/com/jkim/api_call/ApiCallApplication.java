f294babc122392620b0bd73ea9e91dabe0ba76f1
HelloJon29 <jonkim1996@gmail.com>
2023-04-14 05:48:31 -0500

    Setup application.properties for db connection, see readme for connection info, print Hello World to console on app run

--- src/main/java/com/jkim/api_call/ApiCallApplication.java
+++ src/main/java/com/jkim/api_call/ApiCallApplication.java
@@ -8,6 +8,7 @@ public class ApiCallApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiCallApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
